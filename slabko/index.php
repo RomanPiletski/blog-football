6850bf6a61edd89dad73e3f5e79b2f130ec8f942
MaxSl77 <maximslabko77@icloud.com>
2021-11-16 21:16:01 +0300

    commit

---
+++ slabko/index.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+<h1>Личная страница Слабко</h1>
+
+<ul>
+
+    <li>2 занятие - Типы данных, операции
+        <ul>
+            <li><a href="./lesson-2/null.php">Значение null в PHP</a></li>
+            <li><a href="./lesson-2/boolean_values.php">Логические значения в PHP</a></li>
+        </ul>
+    </li>
+
+
+    <li>3 занятие
+        <ul>
+            <li><a href="#">....</a></li>
+            <li><a href="#">...</a></li>
+        </ul>
+    </li>
+
+
+</ul>
+
+
+</body>
+</html>
