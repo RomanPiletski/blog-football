2730eae28dc8c07ab02a9fdab56b90c8cb3d68a3
Roman Piletski <blender-04@yandex.com>
2021-11-17 23:20:09 +0300

    made independent work

--- piletski/index.php
+++ piletski/index.php
@@ -5,32 +5,25 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Roman Piletski</title>
 </head>
 <body>
-
-<h1>Личная страница Романа Пилецкого</h1>
-
-<ul>
-
-    <li>2 занятие - Типы данных, операции
-        <ul>
-            <li><a href="./lesson-2/null.php">Значение null в PHP</a></li>
-<!--            <li><a href="./lesson-2/boolean_values.php">Логические значения в PHP</a></li>-->
-        </ul>
-    </li>
-
-
-    <li>3 занятие
-        <ul>
-            <li><a href="#">....</a></li>
-            <li><a href="#">...</a></li>
-        </ul>
-    </li>
-
-
-</ul>
-
-
+    <h1>Личная страница Романа Пилецкого</h1>
+    <ul>
+        <li>2 занятие - Типы данных, операции
+            <ul>
+                <li><a href="./lesson-2/null.php">Значение null в PHP</a></li>
+                <li><a href="./lesson-2/boolean_values.php">Логические значения в PHP</a></li>
+                <li><a href="./lesson-2/string_values.php">Строки в PHP</a></li>
+                <li><a href="./lesson-2/integer_values.php">Числа и операции над ними в PHP</a></li>
+            </ul>
+        </li>
+        <li>3 занятие
+            <ul>
+                <li><a href="#">....</a></li>
+                <li><a href="#">...</a></li>
+            </ul>
+        </li>
+    </ul>
 </body>
 </html>
