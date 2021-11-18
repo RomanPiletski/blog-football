98a886bdef687a51fee33f77beb2444f58e3d957
Roman Piletski <blender-04@yandex.com>
2021-11-18 08:26:55 +0300

    made homework lesson 2 and styles

--- piletski/index.php
+++ piletski/index.php
@@ -6,7 +6,7 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Roman Piletski</title>
-    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <h1>Личная страница Романа Пилецкого</h1>
