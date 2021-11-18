e8f7884e3614787083d8fdabcc336e09fc03f0ee
Roman Piletski <blender-04@yandex.com>
2021-11-18 08:20:35 +0300

    made homework lesson 2

--- piletski/index.php
+++ piletski/index.php
@@ -11,20 +11,20 @@
 <body>
     <h1>Личная страница Романа Пилецкого</h1>
     <ul>
-        <li>2 занятие - Типы данных, операции
+        <li class="lessons">2 занятие - Типы данных, операции
             <ul>
-                <li><a href="./lesson-2/null.php">Значение null в PHP</a></li>
-                <li><a href="./lesson-2/boolean_values.php">Логические значения в PHP</a></li>
-                <li><a href="./lesson-2/string_values.php">Строки в PHP</a></li>
-                <li><a href="./lesson-2/integer_values.php">Числа и операции над ними в PHP</a></li>
-                <li><a href="./lesson-2/practice_work.php">Практика по работе с переменными и операциями в PHP</a></li>
-                <li><a href="./lesson-2/practice_tag.php">Работа с HTML тегами в PHP</a></li>
+                <li class="lessons_task"><a href="./lesson-2/null.php">Значение null в PHP</a></li>
+                <li class="lessons_task"><a href="./lesson-2/boolean_values.php">Логические значения в PHP</a></li>
+                <li class="lessons_task"><a href="./lesson-2/string_values.php">Строки в PHP</a></li>
+                <li class="lessons_task"><a href="./lesson-2/integer_values.php">Числа и операции над ними в PHP</a></li>
+                <li class="lessons_task"><a href="./lesson-2/practice_work.php">Практика по работе с переменными и операциями в PHP</a></li>
+                <li class="lessons_task"><a href="./lesson-2/practice_tag.php">Работа с HTML тегами в PHP</a></li>
             </ul>
         </li>
-        <li>3 занятие
+        <li class="lessons">3 занятие
             <ul>
-                <li><a href="#">....</a></li>
-                <li><a href="#">...</a></li>
+                <li class="lessons_task"><a href="#"></a></li>
+                <li class="lessons_task"><a href="#"></a></li>
             </ul>
         </li>
     </ul>
