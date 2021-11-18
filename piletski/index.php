f77cac4e60fe14d50b0e879b7efaa219b39d1664
Roman Piletski <blender-04@yandex.com>
2021-11-18 07:59:26 +0300

    made all homework lesson 2

--- piletski/index.php
+++ piletski/index.php
@@ -6,6 +6,7 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Roman Piletski</title>
+    <link rel="stylesheet" href="./css/style.css">
 </head>
 <body>
     <h1>Личная страница Романа Пилецкого</h1>
@@ -17,6 +18,7 @@
                 <li><a href="./lesson-2/string_values.php">Строки в PHP</a></li>
                 <li><a href="./lesson-2/integer_values.php">Числа и операции над ними в PHP</a></li>
                 <li><a href="./lesson-2/practice_work.php">Практика по работе с переменными и операциями в PHP</a></li>
+                <li><a href="./lesson-2/practice_tag.php">Работа с HTML тегами в PHP</a></li>
             </ul>
         </li>
         <li>3 занятие
