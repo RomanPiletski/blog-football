643ac76cedad57e9dab22d6e1af014b12d63810b
Volha Dazhenok <risadapetrovna@gmail.com>
2021-11-19 16:28:12 +0300

    Variables complete

--- Dazhenok/lesson2/variables.php
+++ Dazhenok/lesson2/variables.php
@@ -1,4 +1,55 @@
 //количество секунд в сутках
 <?php
+//Task 1
 echo (60 * 60) * 24;
-?>
+
+//Task 2
+echo ((60 * 60) * 24) * 30;
+
+//Task 3
+echo ((60 * 60) * 24) * 365;
+
+//Task 4
+echo 60 * 24;
+
+//Task 5
+echo (60 * 24) * 365;
+
+//Task 6 - 10
+$Kb = 1024;
+$Mb = $Kb * 1025;
+$Gb = $Mb * 1024;
+$tenGb = $Gb * 10;
+$Tb = $Gb * 1024;
+
+//Task 11
+$r = 5;
+$s = pi() * $r ** 2;
+echo $s;
+
+//Task 12
+$a = 5;
+$s = $a ** 2;
+echo $s;
+
+//Task 13
+$a = 5;
+$b = 4;
+$s = $a * $b;
+echo $s;
+
+//Task 14
+$a = 5;
+$b = 4;
+$p = 2 * ($a + $b);
+echo $p;
+
+//Task 15
+$tc = 25;
+$f = $tc * 1.8 + 32;
+echo $f;
+
+//Task 16
+$f = 70;
+$tc = ($f - 32) / 1.8;
+echo $tc;
