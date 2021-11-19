0917090203c89328996be30b8f155955f424c761
Volha Dazhenok <risadapetrovna@gmail.com>
2021-11-19 14:43:10 +0300

    Strings complete

---
+++ Dazhenok/lesson2/strings.php
@@ -0,0 +1,23 @@
+<?php
+//Task 1
+$str = "hello";
+echo $str, PHP_EOL;
+
+//Task 2
+$a = "abc";
+$b = "efd";
+echo $a.$b, PHP_EOL;
+
+//Task 3
+$c = "Hello,";
+$d = " World!";
+echo $c.$d, PHP_EOL;
+
+//Task 4
+$e = "bablegum";
+echo strlen($e), PHP_EOL;
+
+//Task 5
+$f = "фиолетовенький";
+echo mb_strlen($f), PHP_EOL;
+?>
