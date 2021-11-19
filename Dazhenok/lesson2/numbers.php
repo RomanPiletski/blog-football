13c5f1783a1037af3a75f25fdfeabc8f7702296f
Volha Dazhenok <risadapetrovna@gmail.com>
2021-11-19 15:15:44 +0300

    Numbers complete

---
+++ Dazhenok/lesson2/numbers.php
@@ -0,0 +1,56 @@
+<?php
+//Task 1
+$a = 1 + 2 + 3;
+echo $a;
+
+//Task 2
+echo 1 + 2 + 3;
+
+//Task 3
+$b = 10;
+$c = 2;
+echo $b + $c;
+echo $b - $c;
+echo $b * $c;
+echo $b / $c;
+
+//Task 4
+$d = 10;
+$e = 5;
+$result = $d + $e;
+echo $result;
+
+//Task 5
+$f = 1;
+$g = 2;
+$h = 3;
+echo $f + $g + $h;
+
+//Task 6
+$a = 10;
+$b = 5;
+$c = $a - $b;
+$d = 7;
+$result = $c + $d;
+echo $result;
+
+//Task 19
+$b = 1.5;
+$c = 0.75;
+echo $b + $c;
+
+//Task 20
+$a = -100;
+echo $a;
+
+//Task 21
+$a = 57;
+echo "-".$a;
+
+//Task 22
+$a = 10;
+$b = 3;
+echo $a % $b;
+
+//Task 23
+echo 2 ** 10;
