85d67a340c492dbb43914e51e40b041792eb1029
Volha Dazhenok <risadapetrovna@gmail.com>
2021-11-19 16:54:19 +0300

    HTML tags complete

---
+++ Dazhenok/lesson2/html_tags.php
@@ -0,0 +1,15 @@
+<?php
+//Task 1
+echo '<i>курсивный</i>';
+
+//Task 2
+echo '1<br>2<br>3<br>4<br>5<br>6<br>7<br>8<br>9<br>';
+
+//Task 3
+echo '<img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Ficatcare.org%2Fadvice%2Fthinking-of-getting-a-cat%2F&psig=AOvVaw0fP37ou2RgNKH9utYkNBa7&ust=1637415265101000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCNix6qvFpPQCFQAAAAAdAAAAABAD" alt="cat"><br>';
+
+//Task 4
+echo '<input type="text" name="text" value="Some text"><br>';
+
+//Task 5
+echo '<textarea name="someText" rows="4" cols="50"> Some random text Some random text Some random text Some random text </textarea>';
