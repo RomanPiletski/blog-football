90bd671838e086f0727309ce20c845170121375b
Volha Dazhenok <risadapetrovna@gmail.com>
2021-11-19 14:51:26 +0300

    Strings complete

--- Dazhenok/lesson2/strings.php
+++ Dazhenok/lesson2/strings.php
@@ -6,18 +6,18 @@ echo $str, "<br/>";
 //Task 2
 $a = "abc";
 $b = "efd";
-echo $a.$b, "<br/>";
+echo $a.$b."<br/>";
 
 //Task 3
 $c = "Hello,";
 $d = " World!";
-echo $c.$d, "<br/>";
+echo $c.$d."<br/>";
 
 //Task 4
 $e = "bablegum";
-echo strlen($e), "<br/>";
+echo strlen($e)."<br/>";
 
 //Task 5
 $f = "фиолетовенький";
-echo mb_strlen($f), "<br/>";
+echo mb_strlen($f)."<br/>";
 ?>
