dd2177bd26da2922be2cd17663ce1fae15c9d830
AntonAscheulov <schuca120@mail.ru>
2021-11-19 12:38:08 +0300

    task HTML tags in php done

--- ascheulov/Lesson2_Homework/HTML_tags_in_php.php
+++ ascheulov/Lesson2_Homework/HTML_tags_in_php.php
@@ -1,27 +1,28 @@
-<?php echo 'моя первая программа';
+<?php
+mb_internal_encoding('UTF-8');
+ini_set('display_errors', 'on');
+error_reporting (E_ALL);
 
-echo 'hello <br>';  //dgfbdffhdhb
+// Задача 1 курсивный текст
 
-echo '123 <br>';
+$text1 = '<i>'."курсивный".'</i>'.'<br>';
 
-var_dump('1312313');
+echo $text1;
 
-$a = 10;
-echo $a;
+// Задача 2 числа в столбец
+echo "<br>1<br>2<br>3<br>4<br>5<br>6<br>7<br>8<br>9<br>";
 
-$a = 20;
-echo $a;
+// Задача 3 картинка
 
-echo $a;
+echo '<br><img src="https://proprikol.ru/wp-content/uploads/2020/12/kartinki-ryabchiki-29.jpg" alt="какая=нибудь картинка"><br>';
 
-$b =  !(!545!=(490+55) && !500!=500);
+// Задача 4 input
 
-echo $b;
+echo '<br><input type="text">какой-то текст</input><br>';
 
-$c = !(!(!(431+23)>=454)) || !(23-5!=15+4);
+// Задача 5 textarea
 
-echo $c;
-
+echo '<br><textarea rows="4" cols="50">какой-то текст</textarea><br>'
 ?>
 
 
