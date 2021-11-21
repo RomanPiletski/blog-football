71bcc65405fbe4cf34bc1544ecc11d529513bcca
AntonAscheulov <schuca120@mail.ru>
2021-11-21 14:15:54 +0300

    boolean values practice done

---
+++ ascheulov/practice/lesson2_practice/boolean_values.php
@@ -0,0 +1,9 @@
+<?php
+$isTurue = true;
+
+echo var_dump($isTurue) . " - переменная со значением true <br>";
+
+$isFalse = false;
+
+echo var_dump($isFalse) . " - переменная со значением false";
+?>
