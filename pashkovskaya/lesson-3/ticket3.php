b7b3d97cde3d4f6144d165a75627d4fc3302cafc
unknown <panymari84@gmail.com>
2021-11-24 13:10:03 +0300

    Upload files

--- pashkovskaya/lesson-3/ticket3.php
+++ pashkovskaya/lesson-3/ticket3.php
@@ -4,9 +4,9 @@ echo 'Используя оператор if..else<br>
 $k=-10;
 $z=2;
 $x=0;
-if(k<1){
+if($k<1){
     $x=$k*$z**3;
-}else if(k>=1){
+}else if($k>=1){
     $x=$z*($z+1);
 }
 $y=(log(1+$x**2)+cos($x+1))**M_E**($k*$x);
@@ -15,9 +15,9 @@ echo "First value $y <br>";
 $k=1;
 $z=1;
 if($z>0){
-    $x=1/(z**2+2*z);
+    $x=1/($z**2+2*$z);
 }else if($z<=0){
-    $x=1-z**3;
+    $x=1-$z**3;
 }
-$y=(2*M_E**((-3)*x)-4*x**2)/(log(abs($x))+$x);
+$y=(2*M_E**((-3)*$x)-4*$x**2)/(log(abs($x))+$x);
 echo "Second value $y <br>";
