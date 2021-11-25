d0b26d944edccd14a3267214c23baa9876920996
Nick-Rashkevich <nick_019@mail.ru>
2021-11-26 00:22:48 +0300

    change name of page

--- rashkevich/index.php
+++ rashkevich/index.php
@@ -18,7 +18,7 @@
     <li>3 занятие
         <ul>
             <li><a href="Homeworks\lesson_3\green_card.php">green card</a></li>
-            <li><a href="Homeworks\lesson_3\yellow card.php">yellow card</a></li>
+            <li><a href="Homeworks\lesson_3\yellow_card.php">yellow card</a></li>
             <li><a href="#">red card</a></li>
 
         </ul>
