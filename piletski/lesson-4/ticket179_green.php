a856e9e33c84e061ac196b0792d8a46cc6902377
Roman Piletski <blender-04@yandex.com>
2021-11-30 16:22:12 +0300

    #179 - Задачи по циклам (просто)

--- piletski/lesson-4/ticket179_green.php
+++ piletski/lesson-4/ticket179_green.php
@@ -1,1 +1,14 @@
 <?php
+    $arr = [];
+    for ($i = 1; $i <= 99; $i) {
+        $arr[] = $i;
+        $i = $i + 2;
+    }
+    echo "Через цикл: <pre>";
+    print_r($arr);
+    echo "</pre>";
+    $arr2 = range(1,99,2);
+    echo "Через готовую функцию: <pre>";
+    print_r($arr2);
+    echo "</pre>";
+?>
