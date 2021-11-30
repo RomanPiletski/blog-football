31c2bf2b2abaa7a0411dc3877781a179e566d73c
IlyaPrushak <prushak.ilya.sergeevich@gmail.com>
2021-11-30 21:21:29 +0300

    "[#159]-Array task  (average)"

--- prushak/PHP_Homework_1/php/Array/hw_3_task_4(average).php
+++ prushak/PHP_Homework_1/php/Array/hw_3_task_4(average).php
@@ -2,7 +2,7 @@
 //Используя каждый из циклов (while, foreach, for) - напишите функционал по расчету факториала числа:
 //PS> Факториал - множество всех цифр в числе. Т.е. факториал числа 3 = 1*2*3, факториал числа 5=1*2*3*4*5
 
-function factor($num){
+function factorWithWhile($num){
     $i = 1;
     $fact = null;
     while($i<=$num){
@@ -11,6 +11,28 @@ function factor($num){
     }
     return $fact;
 }
-echo factor(5);
+
+function factorWithFor($num){
+    $fact = null;
+    for($i=1;$i<=$num;$i++){
+        $fact+=$i;
+    }
+    return $fact;
+}
+
+function factorWithForeach($num){
+    $fact = null;
+    $arr=[];
+    $i = 1;
+    while($i<=$num){
+        array_push($arr,$i);
+        $i++;
+    }
+    foreach ($arr as $index){
+        $fact+=$index;
+    }
+    return $fact;
+}
+echo factorWithWhile(5).",".factorWithFor(5).",".factorWithForeach(5);
 
 
