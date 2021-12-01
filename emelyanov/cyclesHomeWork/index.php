fc09815a831b1e2357d4109ff5da7bede9e324b1
SmokeJkee <niikitos2012@gmail.com>
2021-12-01 16:14:46 +0300

    add task factorial

--- emelyanov/cyclesHomeWork/index.php
+++ emelyanov/cyclesHomeWork/index.php
@@ -21,14 +21,22 @@ echo "<br>";
 //Используя каждый из циклов (while, foreach, for) - напишите функционал по расчету факториала числа:
 //PS> Факториал - множество всех цифр в числе. Т.е. факториал числа 3 = 1*2*3, факториал числа 5=1*2*3*4*5
 
-$n = 5;
-$factorial = null;
-$factorialWhile = 1;
-for ($i = 1; $i <= $n; $i++) {
-    $factorial = $i * $n;
+
+$resultFor = 1;
+$factorialFor = 5;
+
+for($i = 1; $i <= $factorialFor; $i++) {
+    $resultFor *= $i;
 }
 
+echo "<br> $resultFor";
 
-echo $factorial;
-echo "<br>";
-echo $factorialWhile;
+$resultWhile = 1;
+$factorialWhile = 5;
+$i = 1;
+while($i <= $factorialWhile) {
+    $resultWhile = $i * $resultWhile;
+    $i++;
+}
+
+echo "<br>$resultWhile";
