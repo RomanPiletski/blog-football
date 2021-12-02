8a6f26172b2d12a3b8b83b0f0174c9b29ca9f218
tmdan <mukhammed.ne@gmail.com>
2021-12-02 21:59:06 +0300

    delete

--- ascheulov/practice/lesson2_practice/boolean_values.php
+++
@@ -1,9 +0,0 @@
-<?php
-$isTurue = true;
-
-echo var_dump($isTurue) . " - переменная со значением true <br>";
-
-$isFalse = false;
-
-echo var_dump($isFalse) . " - переменная со значением false";
-?>
