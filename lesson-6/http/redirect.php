f1b1e04c4dce7c9ba818d4a8edc78bfa5a879140
tmdan <mukhammed.ne@gmail.com>
2021-12-03 18:18:07 +0300

    added

--- lesson-6/http/redirect.php
+++ lesson-6/http/redirect.php
@@ -31,3 +31,9 @@
 
 
 header("Accept-Language: ru-ru,ru;q=0.8,en-us;q=0.5,en;q=0.3");
+
+//Директивы для работы с кешем
+//https://www.php.net/manual/ru/function.header.php
+
+//header("Cache-Control: no-cache, must-revalidate"); // HTTP/1.1
+//header("Expires: Sat, 26 Jul 1997 05:00:00 GMT"); // Дата в прошлом
