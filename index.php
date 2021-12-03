d7e3c885d6d4cadd068e7fd48aca15ece6617bb1
Nick-Rashkevich <nick_019@mail.ru>
2021-12-03 19:31:29 +0300

    check branch

--- index.php
+++ index.php
@@ -11,8 +11,8 @@
 <ul>
     <li>2 занятие - Типы данных, операции
         <ul>
-            <li><a href="./Homeworks/practice_variable.php">Практика по работе с переменными и операциями в PHP</a></li>
-            <li><a href="./Homeworks/practice_html_tags.php">Работа с HTML тегами в PHP</a></li>
+            <li><a href="Homeworks/practice_variable.php">Практика по работе с переменными и операциями в PHP</a></li>
+            <li><a href="Homeworks/practice_html_tags.php">Работа с HTML тегами в PHP</a></li>
         </ul>
     </li>
     <li>3 занятие
