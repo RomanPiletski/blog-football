873e0a6b10d06d91af9ffbabf6703794b716b6b3
Nick-Rashkevich <nick_019@mail.ru>
2021-12-03 16:10:09 +0300

    cards done — #151 #164 #174 #183 #192

--- index.php
+++ index.php
@@ -11,8 +11,8 @@
 <ul>
     <li>2 занятие - Типы данных, операции
         <ul>
-            <li><a href="Homeworks/practice_variable.php">Практика по работе с переменными и операциями в PHP</a></li>
-            <li><a href="Homeworks/practice_html_tags.php">Работа с HTML тегами в PHP</a></li>
+            <li><a href="./Homeworks/practice_variable.php">Практика по работе с переменными и операциями в PHP</a></li>
+            <li><a href="./Homeworks/practice_html_tags.php">Работа с HTML тегами в PHP</a></li>
         </ul>
     </li>
     <li>3 занятие
@@ -20,6 +20,20 @@
         <li><a href="Homeworks\lesson_3\green_card.php">green card</a></li>
             <li><a href="Homeworks\lesson_3\yellow_card.php">yellow card</a></li>
             <li><a href="Homeworks\lesson_3\red_card.php">red card</a></li>
+        </ul>
+    </li>
+    <li>4 занятие — массивы
+        <ul>
+        <li><a href="Homeworks\lesson_4\arrays\easy.php">Задачи по массивам (easy)</a></li>
+        <li><a href="Homeworks\lesson_4\arrays\hard.php">Задачи по массивам (hard)</a></li>
+
+        </ul>
+    </li>
+    <li>4 занятие — циклы
+        <ul>
+        <li><a href="Homeworks\lesson_4\cycles\easy.php">Задачи по циклам (easy)</a></li>
+        <li><a href="Homeworks\lesson_4\cycles\medium.php">Задачи по циклам (medium)</a></li>
+        <li><a href="Homeworks\lesson_4\cycles\hard.php">Задачи по циклам (hard)</a></li>
         </ul>
     </li>
 </ul>
