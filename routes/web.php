90f04f02d617afc3de8c6b2639c252fb2646aedc
igorslepenkov <slepenkov.nii@yandex.by>
2022-03-25 19:32:11 +0300

    Revert "123"
    This reverts commit 3225cf3a76ff4be0aa638501b6badde01be8953c.

--- routes/web.php
+++ routes/web.php
@@ -16,8 +16,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/user/{slug}', [\App\Http\Controllers\UserController::class, 'show'])
-->whereAlpha('slug');
-
-
