cf27914e38bb0a5ca932c940fba42df304ae3f18
Roman Piletski <blender-04@yandex.com>
2022-03-29 00:57:15 +0300

    #523 - Блог(создайте миграции)

---
+++ database/migrations/2022_03_28_194906_create_posts_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string("title", 255);
+            $table->string("slug", 255);
+            $table->text("content");
+            $table->foreignId("category_id")->constrained("categories")->cascadeOnDelete();
+            $table->foreignId("user_id")->constrained("users")->cascadeOnDelete();
+            $table->boolean("is_publish")->default("0");
+            $table->boolean("is_recommended")->default("0");
+            $table->integer("views");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+};
---
+++ database/migrations/2022_03_28_194947_create_comments_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->text("text");
+            $table->string("slug", 255);
+            $table->foreignId("user_id")->constrained("users")->cascadeOnDelete();
+            $table->foreignId("post_id")->constrained("posts")->cascadeOnDelete();
+            $table->boolean("is_publish")->default("0");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+};
