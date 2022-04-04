778cce8a55b1c8956e9029ec4bd636aa24b85e1a
Roman Piletski <blender-04@yandex.com>
2022-04-04 15:03:16 +0300

    #545 - Задача #3 - работа с моделями

--- database/migrations/2022_03_28_194906_create_posts_table.php
+++ database/migrations/2022_03_28_194906_create_posts_table.php
@@ -22,6 +22,7 @@ return new class extends Migration {
             $table->boolean("is_publish")->default(0);
             $table->boolean("is_recommended")->default(0);
             $table->integer("views");
+            $table->string("image", 255)->nullable();
             $table->timestamps();
         });
     }
