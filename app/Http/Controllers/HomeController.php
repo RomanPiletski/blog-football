55d438a8665c72e27481ac70132b360239faf6b5
Roman Piletski <blender-04@yandex.com>
2022-05-14 11:45:15 +0300

    #693 - Задача #16 - Вывод актуальной погоды на сайте через API

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,14 +3,16 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
+use App\Providers\WeatherServiceProvider;
+use App\Services\Weather\Interfaces\WeatherServiceContract;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    public function index()
+    public function index(WeatherServiceContract $weather)
     {
         $posts = Post::paginate(2);
 
-        return view("pages.index", ["posts" => $posts]);
+        return view("pages.index", ["posts" => $posts, "weather" => $weather]);
     }
 }
--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -65,11 +65,11 @@
                     {{$users->links()}}
                 </div>
 
-                <div style="color: #0d6aad; background-color: cornsilk; border: ridge; max-width: 35%; margin: 10px; padding: 10px">
-                    <p>Погода: г. {{$weather->coordinates()->getCityName()}} {{round($weather->temperature()->getCurrentTemperature(),1)}}&deg C.
+                <div style="font-weight:bold; text-align:center; color: #333; background-color: #fff; border: 2px solid #eee; max-width: 100%; margin-bottom: 10px; padding: 10px">
+                    <p>Погода: г. {{mb_strtoupper($weather->coordinates()->getCityName())}} {{round($weather->temperature()->getCurrentTemperature(),1)}}&deg C.
                         Ощущается как {{round($weather->temperature()->getFeelsLikeTemperature(),1)}}&deg C.</p>
-                    <p>{{$weather->cloudiness()->getDescription()}} <img src="{{asset("storage/img/weather/". $weather->cloudiness()->getIcon() . "@2x.png")}}" alt="image-weather"></p>
-                    <p>Влажность {{$weather->temperature()->getHumidity()}} %.
+                    <p style="font-size: 125%;">{{$weather->cloudiness()->getDescription()}} <img src="{{asset("storage/img/weather/". $weather->cloudiness()->getIcon() . "@2x.png")}}" alt="image-weather"></p>
+                    <p style="font-size: 125%;">Влажность {{$weather->temperature()->getHumidity()}} %.
                         Давление {{round($weather->temperature()->getPressure() * 0.75006375541921, 0)}} мм.рт.ст. </p>
                 </div>
 
