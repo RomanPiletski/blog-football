ff15a3514190217a7cd1e3ad8f727c00870df374
Roman Piletski <blender-04@yandex.com>
2022-05-16 13:18:36 +0300

    #### - Задача ### - Боковая панель сайта

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -13,7 +13,7 @@ class HomeController extends Controller
 {
     public function index(WeatherServiceContract $weather)
     {
-        $posts = Post::paginate(2);
+        $posts = Post::orderBy("created_at", "desc")->paginate(2);
 
         return view("pages.index", [
             "posts" => $posts,
